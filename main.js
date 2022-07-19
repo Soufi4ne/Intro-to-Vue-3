456938115e741a90a708ccc424368b91c9d5969e
Soufi4ne <soufiane.le.breton@efrei.net>
2022-07-19 17:47:55 +0200

    dernier changement avant systeme de panier

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const app = Vue.createApp({
     data() { //possibilité de mettre en ES6 data()
         return {
-            product: "Games",
+            product: "Seigneur des Anneaux : Le retour du Roi",
             description: "Le Seigneur des anneaux : Le Retour du Roi est un jeu vidéo de 2003 de type beat them all en vue à la troisième personne, développé par EA Redwood Shores et édité par Electronic Arts.",
             cover: './assets/images/lotrGC.png',
-            url: 'https://en.wikipedia.org/wiki/The_Lord_of_the_Rings:_The_Return_of_the_King_(video_game)'
+            url: 'https://en.wikipedia.org/wiki/The_Lord_of_the_Rings:_The_Return_of_the_King_(video_game)',
+            inStock: false,
+            nombreJoueur:2,
+            details: ['Pegi 12', 'Durée de vie : 20h', 'Rejouabilité : Bonne,', 'Online : Non'],
+            plateformes : [
+                {id:10, console:"GameCube"},
+                {id:11, console:"PlayStaion 2"}
+            ]
         }
     }
 })
